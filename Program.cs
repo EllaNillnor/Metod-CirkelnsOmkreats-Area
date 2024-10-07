0359af7291ed551c93a581ae8ec0d9ca4d13fb13
EllaNillnor <elni06001@utb.vaxjo.se>
2024-10-07 10:26:34 +0200

    Cirkelns omkrets och area

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+double radie=2.4;
+	Console.WriteLine("Cirkelns omkrets: "+CirkelOmkrets(radie));
+	Console.WriteLine("Cirkelns area: "+CirkelArea(radie));
+}
+
+double CirkelOmkrets(double r)
+{
+    double o = r * 2 * Math.PI;
+    return o; 
+}
+
+double CirkelArea(double r)
+{
+    double a = r * r * Math.PI; 
+    return a;
+}
